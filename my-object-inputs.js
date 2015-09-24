585309fbf63ff45cdd227fd9a3eb894333c8a283
Gage Choat <gagechoat@gmail.com>
2015-09-24 15:34:55 -0700

    remove controller from my-object-inputs

--- my-object-inputs.js
+++ my-object-inputs.js
@@ -1,80 +1,24 @@
 myApp.directive('myObjectInputs', function() {
-  var controller = ['$scope', function ($scope) {
-    $scope.transformedObjs = [];
-
-    $scope.showTransformedObjs = function() {
-      console.log('transformedObjs = ');console.dir($scope.transformedObjs);
-    };
-
-    // // add more numbers to this item
-    // $scope.more = function() {
-    //   for ( var i = 0; i < $scope.transformedObjs.length; i++ ) {
-    //     $scope.transformedObjs[i].push({
-    //       order: i,
-    //       number: 0
-    //     })
-    //   }
-    // };
-
-    // // remove numbers from this item
-    // $scope.less = function() {
-    //   // for ( var i = 0; i < $scope.transformedObjs.length; i++ ) {
-    //   //   $scope.transformedObjs[i].pop();
-    //   // }
-    //   if ( $scope.transformedObjs.length > 1 ) {
-    //     var index = $scope.barData.indexOf( bar );
-    //     $scope.barData.splice(index, 1);
-    //   }
-    // };
-
-    // $scope.removeBar = function( bar ) {
-    //     //console.log('removeBar: barData length = ' + $scope.barData.length + ', indexOf bar = ' + $scope.barData.indexOf( bar ) + ', barData (before) = '); console.dir($scope.barData);
-    //     if ( $scope.barData.length > 1 ) {
-    //         var index = $scope.barData.indexOf( bar );
-    //         $scope.barData.splice(index, 1);
-    //     } else {
-    //         $scope.barData = [[
-    //             {
-    //                 'chord':''
-    //             },
-    //             {
-    //                 'chord':''
-    //             },
-    //             {
-    //                 'chord':''
-    //             },
-    //             {
-    //                 'chord':''
-    //             }
-    //         ]];
-    //     }
-    //     //console.log('removeBar: barData (after) = '); console.dir($scope.barData);
-    // };
-
-    // remove numbers from this item
-    $scope.reset = function(arr) {
-      var index = $scope.transformedObjs.indexOf( arr );
-      for (var i = 0; i < $scope.transformedObjs[index].length; i++ ) {
-        $scope.transformedObjs[index][i].number = 0;
-      }
-    };
-  }];
-  
   return {
     restrict: 'E',
-    replace: true,
     require: 'ngModel',
     templateUrl: 'my-object-inputs.html',
-    controller: controller,
     scope: {
-      ngModel: '='
+      model: '=ngModel'
     },
     link: function( $scope, element, attrs, ngModelCtrl ) {
-      
-      // transform to new data format
-      ngModelCtrl.$formatters.push( function(modelValue) {
+      // remove numbers from this item
+      $scope.reset = function(arr) {
+        var index = $scope.transformedObjs.indexOf( arr );
+        for (var i = 0; i < $scope.transformedObjs[index].length; i++ ) {
+          $scope.transformedObjs[index][i].number = 1;
+        }
+
+      };
+
+      var transform = function(modelValue) {
         var transformedData = [[],[]];
-        
+
         for (var i=0; i<modelValue.length; i++) {
           var transformed;
           if (modelValue[i].number == "zero") { transformed = 0 }
@@ -96,16 +40,15 @@ myApp.directive('myObjectInputs', function() {
         }
 
         return transformedData;
-      });
-      
-      // transform back to original data format
-      ngModelCtrl.$parsers.push( function(viewValue) {
+      }
+
+      var untransform = function (viewValue) {
         var untransformedData = [];
-        
+
         for (var i=0; i<viewValue.length; i++) {
           for (var j=0; j<viewValue[i].length; j++) {
             var untransformed;
-            if (viewValue[i][j].number == 0) { untransformed = "zero" }
+            if (viewValue[i][j].number === 0) { untransformed = "zero" }
             else if (viewValue[i][j].number == 1) { untransformed = "one" }
             else if (viewValue[i][j].number == 2) { untransformed = "two" }
             else if (viewValue[i][j].number == 3) { untransformed = "three" }
@@ -119,23 +62,23 @@ myApp.directive('myObjectInputs', function() {
               untransformedData.push({
                 order: viewValue[i][j].order,
                 number: untransformed
-              })
+              });
             }
           }
         }
-      
-        return untransformedData;
-      });
-    
-      // watch for updates to data
-      $scope.$watch('transformedObjs', function() {
-        ngModelCtrl.$setViewValue( angular.copy( $scope.transformedObjs ) );
+
+        return untransformedData; 
+      }
+
+      // watch for updates on parent to data
+      $scope.$watch('model', function() {
+        $scope.transformedObjs = angular.copy(transform($scope.model));
       }, true);
-      
-      // update view
-      ngModelCtrl.$render = function() {
-        $scope.transformedObjs = ngModelCtrl.$viewValue;
-      };
+
+      // watch for updates on directive to data
+      $scope.$watch('transformedObjs', function() {
+        $scope.model = angular.copy(untransform($scope.transformedObjs));
+      }, true);
     }
   }
 });
